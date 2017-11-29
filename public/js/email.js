3994a6a0326d6a8e43958c591d6bf81c6306cd8b
mkinoshi <mkinoshi@colby.edu>
2017-11-29 12:27:51 -0500

    updated

--- public/js/email.js
+++ public/js/email.js
@@ -1,13 +1,23 @@
 $( document ).ready(function() {
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  var today = new Date();
-  var month = (today.getMonth() + 1 ) + "/" + today.getDate();
-  var ind = today.getDay();
-  var day = days[ind]
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  $("#day")[0].innerHTML = month + " - " + h + ":" + m + ":" + s; 
+  // var today = new Date();
+  // var month = (today.getMonth() + 1 ) + "/" + today.getDate();
+  // var ind = today.getDay();
+  // var day = days[ind]
+  // var h = today.getHours();
+  // var m = today.getMinutes();
+  // var s = today.getSeconds();
+  setInterval(function() {
+    var today = new Date();
+    var month = (today.getMonth() + 1 ) + "/" + today.getDate();
+    var ind = today.getDay();
+    var day = days[ind]
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    console.log(today)
+    $("#day")[0].innerHTML = month + " - " + h + ":" + m + ":" + s; 
+  }, 1000)
 
-  
+
 });
--- public/js/initial.js
+++ public/js/initial.js
@@ -7,5 +7,15 @@ $( document ).ready(function() {
   var h = today.getHours();
   var m = today.getMinutes();
   var s = today.getSeconds();
-  $("#day")[0].innerHTML = month + " - " + h + ":" + m + ":" + s; 
+  setTimeInterval(function() {
+    var today = new Date();
+    var month = (today.getMonth() + 1 ) + "/" + today.getDate();
+    var ind = today.getDay();
+    var day = days[ind]
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    console.log(today)
+    $("#day")[0].innerHTML = month + " - " + h + ":" + m + ":" + s; 
+  }, 1000)
 });
