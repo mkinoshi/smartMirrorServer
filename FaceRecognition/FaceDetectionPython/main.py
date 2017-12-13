0a7848caf0a8249b9fa5aa8b28470db05b09f795
anantkes19 <anantkes@colby.edu>
2017-12-12 22:18:46 -0500

    Adding Final Presentation Content

--- FaceRecognition/FaceDetectionPython/main.py
+++ FaceRecognition/FaceDetectionPython/main.py
@@ -11,12 +11,12 @@ import requests
 
 def get_images_and_labels(folderpath):
 	'''
-		Returns a list of images and a list of labels for the subject of those images. 
-		Initially, the labels will all be '1' as the system will be tested using only one 
-		user 
+		Returns a list of images and a list of labels for the subject of those images.
+		Initially, the labels will all be '1' as the system will be tested using only one
+		user
 
 		folderpath is the path of the folder where the images are kept relative to the folder
-		from where the user's main program is being run 
+		from where the user's main program is being run
 	'''
 
 	#load pre-built opencv XML trained classifiers
@@ -30,7 +30,7 @@ def get_images_and_labels(folderpath):
 		#read all of the images containing jerry's face
 		src = cv2.imread(basic_paths+ '%02d.jpg' % (i), cv2.IMREAD_GRAYSCALE )
 		#jerry's face (ID 01) is the only recognized
-		label = 1 
+		label = 1
 
 		faces = face_cascade.detectMultiScale(src, minSize = (200,200))
 
@@ -46,7 +46,7 @@ def sendFound():
 		sends a message to the server hosting the mirror webpage telling it that the user has been found
 	'''
 	try :
-		r = requests.post("http://137.146.139.43:3000/detected/", data={'user': 'Jerry'})
+		r = requests.post("http://127.0.0.1:3000/detected/", data={'user': 'Jerry'})
 	except:
 		print "CANNOT CONNECT TO SERVER -- Detection Data not Sent"
 
@@ -56,41 +56,41 @@ def sendLost():
 		sends a message to the server hosting the mirror webpage telling it that the user has been found
 	'''
 	try:
-		r = requests.post("http://137.146.139.43:3000/lost/", data={'user': 'Jerry'})
+		r = requests.post("http://127.0.0.1:3000/lost/", data={'user': 'Jerry'})
 	except:
 		print "CANNOT CONNECT TO SERVER -- LOST FACE DATA NOT SENT"
 
 def scanForFace():
 
 	'''
-		scanForFace initializes the facial recognition mechanisms in the SETUP phase. Then, it enters an infinite loop 
-		during which it takes in the current frame coming from the video feed. It scans that frame for faces using opencv's 
-		detectmultiScale function and an existing trained haar cascade classifier. 
+		scanForFace initializes the facial recognition mechanisms in the SETUP phase. Then, it enters an infinite loop
+		during which it takes in the current frame coming from the video feed. It scans that frame for faces using opencv's
+		detectmultiScale function and an existing trained haar cascade classifier.
 
-		If a face is found, then the face is processed by a face recognizer which returns a 
-		confidence floating point value. This value is the distance from the face that the 
-		recognizer has been trained to identify. If the value is below a certain threshold 
+		If a face is found, then the face is processed by a face recognizer which returns a
+		confidence floating point value. This value is the distance from the face that the
+		recognizer has been trained to identify. If the value is below a certain threshold
 		(in the current version of the software the threshold is 195.0), then the function
-		acknowledges the face being scanned as a valid user's face. The program will then 
+		acknowledges the face being scanned as a valid user's face. The program will then
 		go over 35 cycles of face recognition and then find the percentage of times that a valid
-		face was scanned. If on average, a valid face was scanned over 60% of the time, then 
+		face was scanned. If on average, a valid face was scanned over 60% of the time, then
 		the program will call on the sendFound function to tell the information server that
-		a valid user is in front of the camera. 
+		a valid user is in front of the camera.
 
-		Lastly, if no user is recognized for 30 seconds after accessing the information screen, 
-		another message will be sent to the server to stop displaying the user's information  
-	
+		Lastly, if no user is recognized for 30 seconds after accessing the information screen,
+		another message will be sent to the server to stop displaying the user's information
+
 	'''
 
 	#---- SETUP OPENCV COMPONENTS --------------------------------------------------------
 	#get the lists of images and labels from the appropriate folder
 	images, labels = get_images_and_labels("../FaceDatabase")
-	
+
 	#train a face recognizer using the images and labels
 	recognizer = cv2.face.createLBPHFaceRecognizer(grid_x=12, grid_y=12)
 	recognizer.train(images, np.array(labels))
 
-	#train a general face detecting casade 
+	#train a general face detecting casade
 	face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
 	#set up video capture
@@ -101,20 +101,20 @@ def scanForFace():
 	cv2.setWindowProperty('src',cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 	#------------------------------------------------------------------
 
-	
+
 	#initialize variables
 	loopCount = 0
 	total_sum = 0
 	start_time = time.time()
-	found = False 
-	
-	
+	found = False
+
+
 	#infinite loop and logic
 	while True:
-		
+
 		#read the current frame
 		retval, src = cap.read()
-		
+
 		#no image could be obtained from the camera
 		if not retval:
 			print "CANNOT READ FRAME FROM VideoCapture"
@@ -123,7 +123,7 @@ def scanForFace():
 		#get faces in the frame and handle the case with no faces
 		gray_img = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY )
 		faces = face_cascade.detectMultiScale(gray_img,1.3,minSize = (140,140),  maxSize=(180,180))
-		
+
 		if faces == ():
 			cv2.imshow('src', src)
 			total_sum += 0
@@ -131,7 +131,7 @@ def scanForFace():
 
 		#if a face is found
 		else:
-			
+
 			for face in faces:
 
 				x = face[0]
@@ -139,10 +139,10 @@ def scanForFace():
 				w = face[2]
 				h = face[3]
 
-				#display a red rectangle around the face if over a 
+				#display a red rectangle around the face if over a
 				#confidence threshold of 195.0 or green if under that same threshold
 				predicted, conf = recognizer.predict(gray_img[y: y + h, x: x+ w])
-				
+
 				if conf < 195.0:
 					color = (0,255,0)
 					total_sum += 1
@@ -159,15 +159,15 @@ def scanForFace():
 '''
 
 if loopCount == 35:
-			
+
 			print total_sum/35.0
 
 			if (total_sum/35.0) > 0.6:
-				
+
 				print 'user found at %s seconds' % time.time()
 
-				found = True 
-				lost = False 
+				found = True
+				lost = False
 				r = requests.post("http://127.0.0.1:3000/detected/", data={'user': 'Jerry'})
 				start_time = time.time()
 
@@ -178,11 +178,11 @@ if loopCount == 35:
 					r = requests.post("http://127.0.0.1:3000/lost/", data={'user': 'Jerry'})
 					print 'user lost at %s seconds' % time.time()
 
-			total_sum = 0		
+			total_sum = 0
 			loopCount = 0
 
-Above code is the original 'dirty' version. I refactored it by extracting two methods sendFound and sendLost. 
-I also more clearly commented them and cleaned up the logic by eliminating an unnecessary boolean variable 
+Above code is the original 'dirty' version. I refactored it by extracting two methods sendFound and sendLost.
+I also more clearly commented them and cleaned up the logic by eliminating an unnecessary boolean variable
 
 
 
@@ -199,34 +199,33 @@ I also more clearly commented them and cleaned up the logic by eliminating an un
 			#if a valid user is found over 60% of the time then reset the wait timer
 			#and if the found message has not been sent, send it
 			if (total_sum/35.0) > 0.6:
-				
+
 				start_time = time.time()
 
 				if found == False:
-					#send a message 
-					print 'user found' 
-					found = True 
+					#send a message
+					print 'user found'
+					found = True
 					sendFound()
-				
+
 			#if the user has been found and there's been over thirty seconds without having
-			#found them again, then send a lost message 
+			#found them again, then send a lost message
 			elif found == True:
 				if (time.time() - start_time) > 30.0:
 					found = False
 					sendLost()
 					print 'user lost'
 
-			total_sum = 0		
+			total_sum = 0
 			loopCount = 0
-				
-				
 
-	
-		
-		
+
+
+
+
+
 
 
 if __name__ == "__main__":
 	#ensures we can use these functions in the future
 	scanForFace()
-	
---
+++ readme.txt
@@ -0,0 +1,18 @@
+This project is intended for the Ubuntu platform
+
+Installation of Software:
+
+1. Install NodeJs with the following in a command prompt: sudo apt-get install node
+2. Clone the git hub with the following: git clone https://github.com/mkinoshi/smartMirrorServer
+3. Cd into the directory
+4. Update the packages for node: npm install
+5. Start the node server with: node app.js
+6. Install opencv3.2.0 by following these steps listed here: http://www.samontab.com/web/2017/06/installing-opencv-3-2-0-with-contrib-modules-in-ubuntu-16-04-lts/
+7. Cd into the python directory
+8. Start the python client with: python main.py
+9. Open a web browser to localhost:3000
+10. Once there, if the camera detects the correct face it will take the user to localhost:3000/email
+
+
+Optional:
+If you do not look like Gerardo Diaz (The correct face), with your web browser go to localhost:3000/email manually by typing it into the url, it will display all of the info
